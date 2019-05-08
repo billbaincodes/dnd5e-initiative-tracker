d8b602e8058be0fdaa13ba75fd6ac249abfa491f
bill <williamandrewbain@gmail.com>
2019-05-07 18:04:02 -0600

    Correct specialability component

---
+++ src/components/SpecialAbility.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const SpecialAbility = ({ data }) => {
+  return(
+    <div className="action">
+      <div><b>{data.name}</b></div>
+      <div><b>Description:</b> <span className="small-text">{data.desc}</span></div>
+      <div><b>Atk Bonus:</b> {data.attack_bonus}</div>
+    </div>
+  )
+}
+
+export default SpecialAbility
